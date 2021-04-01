dd1566c9ac1655252a4bf972192521d93fc6bc60
Marton Hun <hunmarton@hotmail.com>
2021-04-01 14:20:42 +0200

    Starter Templeate

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import carousel from "../node_modules/bootstrap/js/dist/carousel";
+import collapse from "../node_modules/bootstrap/js/dist/collapse";
+import "./style.scss";
+import "./Jquery/test";
